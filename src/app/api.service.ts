a95af4e63b580f998c31b0827e1c03b4fc1d6439
jeswi2 <jeswimathewm@gmail.com>
2022-10-30 20:13:39 +0530

    sec

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -28,7 +28,7 @@ export class ApiService {
     return this.http.post("http://localhost:8080/edit",data)
   }
 
-  secuAdd=(data:any)=>{
+  secuadd=(data:any)=>{
     return this.http.post("http://localhost:8080/addsecurity",data)
   }
 }
