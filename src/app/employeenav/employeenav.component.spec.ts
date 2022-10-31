b9de7916a48672ecfa3a7d5ad811403cb5f6a73a
jeswi2 <jeswimathewm@gmail.com>
2022-10-31 14:05:26 +0530

    emnav

---
+++ src/app/employeenav/employeenav.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmployeenavComponent } from './employeenav.component';
+
+describe('EmployeenavComponent', () => {
+  let component: EmployeenavComponent;
+  let fixture: ComponentFixture<EmployeenavComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EmployeenavComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EmployeenavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
