37fb0d71f32e6f5fa949e88d1b5a107f48ea4005
jeswi2 <jeswimathewm@gmail.com>
2022-11-01 00:31:17 +0530

    fw

--- src/app/adminnavbar/adminnavbar.component.ts
+++ src/app/adminnavbar/adminnavbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adminnavbar',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminnavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myRouter:Router) { }
+
+  logout=()=>{
+    localStorage.clear
+    this.myRouter.navigate(["/"])
+  }
 
   ngOnInit(): void {
   }
--- src/app/employeenav/employeenav.component.ts
+++ src/app/employeenav/employeenav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employeenav',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeenavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myRouter:Router) { }
+
+  logout=()=>{
+    localStorage.clear
+    this.myRouter.navigate(["/employee"])
+  }
 
   ngOnInit(): void {
   }
